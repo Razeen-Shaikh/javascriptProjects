d30040430751ed3e4548f7a7436d42da0aa26560
Razeen <34880700+Razeen-Shaikh@users.noreply.github.com>
2021-10-18 21:45:54 +0530

    Updated Color Flipper Project

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -2,14 +2,7 @@ const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-color.textContent = "green";
-
-btn.addEventListener("click", function () {
-	let hexColor = "#";
-	for (let i = 0; i < 6; i++) {
-		let index = getRandomNumber();
-		hexColor += colors[index];
-	}
+const getNameOfColor = (hexColor) => {
 	var n_match = ntc.name(hexColor);
 	n_rgb = n_match[0]; // RGB value of closest match
 	n_name = n_match[1]; // Text string: Color name
@@ -18,23 +11,19 @@ btn.addEventListener("click", function () {
 	color.textContent = n_name;
 	color.style.color = hexColor;
 	document.body.style.backgroundColor = hexColor;
+};
+
+getNameOfColor("#f1f5f8");
+
+btn.addEventListener("click", function () {
+	let hexColor = "#";
+	for (let i = 0; i < 6; i++) {
+		let index = getRandomNumber();
+		hexColor += colors[index];
+	}
+	getNameOfColor(hexColor);
 });
 
 const getRandomNumber = () => {
 	return Math.floor(Math.random() * colors.length);
 };
-
-// const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-// const btn = document.getElementById("btn");
-// const color = document.querySelector(".color");
-
-// btn.addEventListener("click", function () {
-// 	const randomNumber = getRandomNumber();
-
-// 	document.body.style.backgroundColor = colors[randomNumber];
-// 	color.textContent = colors[randomNumber];
-// });
-
-// function getRandomNumber() {
-// 	return Math.floor(Math.random() * colors.length);
-// }
